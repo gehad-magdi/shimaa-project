cae454767addf5660c74b01e46e23f33d9e1bda5
gehad-magdi <55022728+gehad-magdi@users.noreply.github.com>
2020-07-20 05:06:32 -0700

    the form now is working

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -29,7 +29,7 @@ $CT_Main_0_ctl10_drpValue = strip_tags(htmlspecialchars($_POST['phone']));
 
 
 // Create the email and send the message
-$to = 'gehadmagdi1990@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@kangmoving.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message\n\nAdress: $adress\n\nMoving: $moving\n\nPostal: $Postal\n\nCT_Main_0_ctl16_drpValue: $CT_Main_0_ctl16_drpValue\n\nCity: $City\n\nCT_Main_0_ctl10_drpValue: $CT_Main_0_ctl10_drpValue";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
